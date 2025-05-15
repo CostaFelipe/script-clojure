0721b24d13fc6b02b2fb55779d33dbbc40507898
wedo <costadefelipe@gmail.com>
2025-05-15 14:09:47 -0300

    arit, str, numbers, variables

---
+++ numbers.clj
@@ -0,0 +1,13 @@
+(type 120)
+
+(type 1.2)
+
+(type Math/PI)
+
+(type (/ 1 2))
+
+(type (double 32))
+
+(type (/ 64 (double 32)))
+
+(type (long 3.1415))
---
+++ strings.clj
@@ -0,0 +1,13 @@
+(require '[clojure.string :as str])
+
+(print "Hello World!")
+
+(type "Hello")
+
+(str "1 + 2 = " (+ 1 2))
+
+(count "Testing")
+
+(str/reverse "Hello")
+
+(str/replace "Hello" #"l" "1")
