c24d28cf9dc340849f5e29004fac19f1db69add5
wedo <costadefelipe@gmail.com>
2025-05-16 13:49:32 -0300

    if, when, do

--- conditions_and_branching.clj
+++ conditions_and_branching.clj
@@ -17,4 +17,45 @@
       (println "is negative")
       (println "is neither positive nor negative"))))
 
+(defn verificar-idade [idade]
+  (if (>= idade 18)
+    "maior idade"
+    "menor idade"))
+
 ;;do
+(let [a -700]
+  (if (> a 0)
+    (do
+      (println a "is positive")
+      (println "There are infinite positive numbers"))
+    (do
+      (println a "is negative")
+      (println "the are infinite negative numbers"))))
+
+(defn inicializar-sistema []
+  (do
+    (println "Carregar configuração...")
+    (println "conectar ao banco de dados")
+    "Sistema inicializado com sucesso"))
+
+;;when
+(def usuario {:name "Jhon doe" :email "teste@gmail.com"})
+
+(defn salvando-usuario [usuario]
+  (println "Salvando usuario" (:name usuario)) )
+
+(defn registrar-usuario [usuario]
+  (when (:email usuario)
+     (println "Enviando email de boas vindas")
+     (println (salvando-usuario usuario))
+     true))
+
+(registrar-usuario usuario)
+
+
+;;cond
+(
+ comment
+ (verificar-idade 17)
+ (inicializar-sistema)
+ )
