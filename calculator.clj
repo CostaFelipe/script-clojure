e91bc36aa8309debf193b2e4ea1b911c70124d6e
wedo <costadefelipe@gmail.com>
2025-05-21 13:36:04 -0300

    create convert for string to int

--- calculator.clj
+++ calculator.clj
@@ -1,11 +1,16 @@
+(defn convert [c]
+  (if (string? c)
+    (read-string c)
+    (int c)))
+
 (defn operation [a b operator]
   (case operator
-    :add (+ a b)
+    :add (+ (convert a) (convert b))
     :sub (- a b)
     :mult (* a b)
     :div (/ a b)))
 
-(operation 3 3 :sub)
 (operation 3 3 :add)
+(operation "100" "1" :add)
 (operation 6 3 :mult)
 (operation 9 3 :div)
