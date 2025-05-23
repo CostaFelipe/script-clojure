88d07a1befe8a507a7ad7a7848f08c26fa9b38a8
wedo <costadefelipe@gmail.com>
2025-05-23 10:11:48 -0300

    verificando mapas

--- spec.clj
+++ spec.clj
@@ -15,9 +15,8 @@
 
 (defn string-or-keyword-um? [a]
   (if (or (string? a) (keyword? a))
-      true
-      false
-    ))
+    true
+    false))
 
 (s/valid? string-or-keyword-um? 101)
 (s/valid? string-or-keyword-um? :abc)
@@ -41,3 +40,37 @@
   (s/or :number number? :string string?))
 
 (s/valid? number-or-string? 1)
+
+(def number-or-string-colletion?
+  (s/coll-of number-or-string?))
+
+(s/valid? number-or-string-colletion? [1 "Hi" "India" 42])
+
+(s/valid? number-or-string-colletion? [1 "Hi" "India" 42 :mint])
+
+(def valid-person-vector?
+  (s/cat :name string? :age number? :gender keyword?))
+
+(s/valid? valid-person-vector? ["Johnny B Goode" 40 :male])
+
+;;valid map
+(def valid-person-map?
+  (s/keys :req-un [::name
+                   ::age
+                   ::gender]))
+
+(s/valid? valid-person-map? {:name "Johnny B Goode"
+                             :age 45
+                             :gender :male})
+
+
+;;verificando mapas
+(s/def ::name string?)
+
+(s/valid? ::name "Johnny Doe")
+
+(s/def ::user (s/keys :req-un [::name ::age ::gender]))
+
+(s/valid? ::user {:name "Johnny B Goode"
+                             :age 45
+                             :gender :male})
