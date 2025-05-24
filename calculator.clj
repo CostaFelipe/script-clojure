0fc0cc987e219122043bd83537c6f164fda410bb
wedo <costadefelipe@gmail.com>
2025-05-23 23:26:23 -0300

    refatorando calculator.cls

--- calculator.clj
+++ calculator.clj
@@ -1,16 +1,19 @@
 (defn valid-number? [a b]
   (and (number? a) (number? b)))
 
+(defn apply-operator [a b operator]
+  (case operator
+    :add (+ a b)
+    :sub (- a b)
+    :mult (* a b)
+    :div (/ a b)
+    :not_exist)
+  )
+
 (defn operation [a b operator]
   (if (valid-number? a b)
-    (case operator
-      :add (+ a b)
-      :sub (- a b)
-      :mult (* a b)
-      :div (/ a b)
-      :not_exist)
-     "Não não pode colocar string"
-    ))
+    (apply-operator a b operator)
+    "Não não pode colocar string"))
 
 (operation "101" 1 :sub)
 (operation 3 3 :add)
