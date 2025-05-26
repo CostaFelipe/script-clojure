0df24d771381c030b69c4ee20855d95c8d107012
wedo <costadefelipe@gmail.com>
2025-05-26 09:39:17 -0300

    lazy sequence

---
+++ lazy_sequence_repeat.clj
@@ -0,0 +1,13 @@
+(def multiverse (repeat "universe"))
+
+(first multiverse)
+
+(take 5 multiverse)
+
+(nth multiverse 100)
+
+(repeat 5 "universe")
+
+(nth (repeat 5 "universe") 3)
+
+(last (repeat 5 "universe"))
