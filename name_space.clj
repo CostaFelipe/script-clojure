359cf2c3145e53f9ade1a219268625b2bd43d10c
wedo <costadefelipe@gmail.com>
2025-05-28 14:21:13 -0300

    name space

---
+++ name_space.clj
@@ -0,0 +1,15 @@
+(ns lawyer)
+
+(def about-me "I speak truth")
+
+(ns scientist)
+
+(def about-me "I creater new language")
+
+(ns engineer)
+
+(def about-me "I apply science")
+
+(println lawyer/about-me)
+(println scientist/about-me)
+(println engineer/about-me)
