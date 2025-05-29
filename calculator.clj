7d26920a6638b954eef4c2b7e50e7c41cca175d3
wedo <costadefelipe@gmail.com>
2025-05-29 09:59:53 -0300

    create test for calculator.clj

--- calculator.clj
+++ calculator.clj
@@ -14,10 +14,3 @@
   (if (valid-number? a b)
     (apply-operator a b operator)
     "Não não pode colocar string"))
-
-(operation "101" 1 :sub)
-(operation 3 3 :add)
-(operation "100" "1" :add)
-(operation 6 3 :mult)
-(operation 9 3 :div)
-(operation 2 2 :sos)
