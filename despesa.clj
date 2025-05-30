48b37a35ad446aa9267323af42fefa73685b637a
wedo <costadefelipe@gmail.com>
2025-05-30 09:15:00 -0300

    treinando clojure despesa

--- despesa.clj
+++ despesa.clj
@@ -7,3 +7,28 @@
   (select-keys transacao [:valor :tipo]))
 
 (map resumo transacoes)
+
+(defn despesa? [transacao]
+  (= (:tipo transacao) "despesa"))
+
+(defn so-valor [transacao]
+  (:valor transacao))
+
+(filter despesa? transacoes)
+
+(map so-valor (filter despesa? transacoes))
+
+(reduce + (map so-valor (filter despesa? transacoes)))
+
+(defn valor-grande? [transacao]
+  (> (:valor transacao) 100))
+
+(filter valor-grande? transacoes)
+
+(so-valor (first transacoes))
+
+(-> (first transacoes) (so-valor))
+
+(->> (filter despesa? transacoes)
+     (map so-valor)
+     (reduce +))
