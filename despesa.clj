299e44b2cea2e76bfe55817687248595647aaad0
wedo <costadefelipe@gmail.com>
2025-05-30 17:54:13 -0300

    refatorando:corrigindo ponto flutuante

--- despesa.clj
+++ despesa.clj
@@ -1,12 +1,16 @@
+;;data
 (def transacoes
-  [{:valor 33.0 :tipo "despesa" :comentario "almoço" :moeda "R$" :data "20/03/2025"}
-   {:valor 2700.0 :tipo "receita" :comentario "bico" :moeda "R$" :data "21/03/2025"}
-   {:valor 89.9 :tipo "despesa" :comentario "Livro golang" :moeda "R$" :data "20/04/2025"}])
+  [{:valor 33.0M :tipo "despesa" :comentario "almoço" :moeda "R$" :data "20/03/2025"}
+   {:valor 2700.0M :tipo "receita" :comentario "bico" :moeda "R$" :data "21/03/2025"}
+   {:valor 89.9M :tipo "despesa" :comentario "Livro golang" :moeda "R$" :data "20/04/2025"}])
 
+(def cotacoes
+  {:yuan {:cotacao 2.15M :simbolo "¥"}
+   :euro {:cotacao 6.49M :simbolo "€"}})
+
+;;functions
 (defn resumo [transacao]
-  (select-keys transacao [:valor :tipo]))
-
-(map resumo transacoes)
+  (select-keys transacao [:valor :tipo :data]))
 
 (defn despesa? [transacao]
   (= (:tipo transacao) "despesa"))
@@ -14,24 +18,8 @@
 (defn so-valor [transacao]
   (:valor transacao))
 
-(filter despesa? transacoes)
-
-(map so-valor (filter despesa? transacoes))
-
-(reduce + (map so-valor (filter despesa? transacoes)))
-
 (defn valor-grande? [transacao]
   (> (:valor transacao) 100))
-
-(filter valor-grande? transacoes)
-
-(so-valor (first transacoes))
-
-(-> (first transacoes) (so-valor))
-
-(->> (filter despesa? transacoes)
-     (map so-valor)
-     (reduce +))
 
 (defn valor-sinalizado [transacao]
   (let [moeda (:moeda transacao "R$")
@@ -39,6 +27,49 @@
     (if (= (:tipo transacao) "despesa")
       (str moeda " -" valor)
       (str moeda " +" valor))))
+
+(defn data-valor [transacao]
+  (str (:data transacao) " => " (valor-sinalizado transacao)))
+
+(defn transacao-em-yuan [transacao]
+  (let [yuan (:yuan cotacoes)]
+    (assoc transacao :valor (* (:cotacao yuan) (:valor transacao))
+           :moeda (:simbolo yuan))))
+
+(defn texto-resumo-em-yuan [transacao]
+  (-> (transacao-em-yuan transacao)
+      (data-valor)))
+
+;;executing
+
+(map resumo transacoes)
+
+(so-valor (first transacoes))
+
+(filter despesa? transacoes)
+
+(map so-valor (filter despesa? transacoes))
+
+(filter valor-grande? transacoes)
+
+(-> (first transacoes) (so-valor))
+
+(->> (filter despesa? transacoes)
+     (map so-valor)
+     (reduce +))
+
+;;bricando de acesso
+(:cotacao (:euro cotacoes))
+(:simbolo (:euro cotacoes))
+
+;;aplicando destructions
+(let [euro (:euro cotacoes)]
+  (:simbolo euro))
+
+(let [yuan (:yuan cotacoes)]
+  (:cotacao yuan))
+
+(reduce + (map so-valor (filter despesa? transacoes)))
 
 (valor-sinalizado (first transacoes))
 
@@ -48,9 +79,12 @@
 
 (valor-sinalizado transacao-aleatoria)
 
-(defn data-valor [transacao]
-  (str (:data transacao) " => " (valor-sinalizado transacao)))
-
 (data-valor (first transacoes))
 
 (map data-valor transacoes)
+
+(transacao-em-yuan (first transacoes))
+
+(data-valor (transacao-em-yuan (first transacoes)))
+
+(map texto-resumo-em-yuan transacoes)
