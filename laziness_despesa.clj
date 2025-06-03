8ed6add5674233fea0d0cba2b022162d744648e4
wedo <costadefelipe@gmail.com>
2025-06-03 19:29:35 -0300

    lazy-seq

--- laziness_despesa.clj
+++ laziness_despesa.clj
@@ -12,3 +12,30 @@
   (repeatedly transacao-aleatoria))
 
 (take 5 transacoes-aleatorias)
+
+(= (take 5 transacoes-aleatorias) (take 5 transacoes-aleatorias))
+
+;;macro lazy-seq
+(cons (transacao-aleatoria) transacoes)
+
+(defn aleatorias
+  ([quantidade] (aleatorias quantidade 1 (list (transacao-aleatoria))))
+  ([quantidade quantas-ja-foram transacoes]
+   (if (= quantas-ja-foram quantidade)
+     transacoes
+     (recur quantidade (inc quantas-ja-foram)
+            (cons (transacao-aleatoria) transacoes)))))
+
+(aleatorias 4)
+
+(time (class (aleatorias 900000)))
+
+(defn aleatorias-lazy []
+  (lazy-seq
+   (cons (transacao-aleatoria) (aleatorias-lazy))))
+
+(time (class (take 4 (aleatorias-lazy))))
+
+(time (class (take 900000 (aleatorias-lazy))))
+
+(take 4 (aleatorias-lazy))
