dfe9ab78f66ffdd7cefce95b911f494cf11033e3
wedo <costadefelipe@gmail.com>
2025-06-04 15:34:19 -0300

    refatorando operacoes

--- calculadora/src/calculadora/operacoes/operacao.clj
+++ calculadora/src/calculadora/operacoes/operacao.clj
@@ -3,9 +3,13 @@
 (defn valid-number? [a b]
   (and (number? a) (number? b)))
 
+
+(defn- sum [a b]
+  (+ a b))
+
 (defn apply-operator [a b operator]
   (case operator
-    :add (+ a b)
+    :add (sum a b)
     :sub (- a b)
     :mult (* a b)
     :div (/ a b)
