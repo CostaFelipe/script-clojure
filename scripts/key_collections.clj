cd4f993490141a8791b8b0ef3d5656c14d13a44f
wedo <costadefelipe@gmail.com>
2025-06-19 15:05:34 -0300

    [count get conj counted? cons first second rest next]

--- scripts/key_collections.clj
+++ scripts/key_collections.clj
@@ -10,12 +10,16 @@
 
 (array-map :language "Elixir" :creator "José Valim")
 
+(hash-map :name "Teste" :age 22)
+
 ;{:language "C++" :creator}
 
 (conj {:language "Elixir"} [:creator "José Valim"])
 
 ;;vector
 [1 2 3 4 5]
+
+(vector 1 2 3 4 5 6 7)
 
 ["clojure" "scala" "erlang" "haskell" "elixir"]
 
@@ -47,3 +51,103 @@
 #{"clojure" "scala" "erlang" "haskell" "elixir", "golang"}
 
 (conj #{:tea :coffe} :juice)
+
+;seq, cons, list*
+(seq [1 2 3 4])
+
+(seq {:languages "elixir" :creator "José Valim"})
+
+(seq '(1 2 3 4 5))
+
+(seq #{"clojure" "scala" "erlang" "haskell" "elixir", "golang"})
+
+(seq [])
+
+(def xs [1 2 3])
+
+(if (seq xs)
+  (println "Do something  with this sequence")
+  (println "Do something else"))
+
+(def user {:username "User01" :email "email@.com"})
+
+(if (seq user)
+  (println "Do something  with this sequence")
+  (println "Do something else"))
+
+(cons 0 (range 1 3))
+
+(list* 0 1 (range 2 5))
+
+;first, rest, next
+(first (seq [1 2 3 4 5 6 7]))
+
+(rest (seq [1 2 3 4 5]))
+
+(next (seq [1 2 3 4 5 6 7 8]))
+
+(rest (seq [:one]))
+
+(next (seq [:one]))
+
+(first [1 2 3 4 5 6 7 8 9])
+
+(second [1 2 3 4 5 6 7 8 9])
+
+(rest [1 2 3 4 5 6 7 8 9])
+
+(next [1 2 3 4 5 6 7 8 9] )
+
+;Key Operations on Collections and Sequences
+
+;count
+(count [1 2 3 4 5])
+
+(count [])
+
+(count nil)
+
+(counted? "String")
+
+(counted? [1 2 3 4 5])
+
+(count "hello")
+
+(counted? (range))
+
+;conj
+(conj '(1 2) 3)
+
+(conj [1 2] 3)
+
+(conj {:a 1 :b 2 :c :3} [:d 4])
+
+(conj {:cats 1 :dogs 2} [:ants 400] [:giraffes 13])
+
+(conj #{1 4} 5)
+
+(conj #{:a :b :c} :b :c :d :e)
+
+;;get
+
+;maps
+(get {:a 1 :b 2 :c 3} :b)
+
+;index of a vector
+(get [10 15 20 25] 2)
+
+;sets
+(get #{1 10 100 2 20 200} 100)
+
+(get #{:orange :strawberry} :apple)
+
+;get in maps
+(get {:name "Test" :email "test@test.com"} :age "Not found")
+
+(get {:name "Test" :email "test@test.com"} :email "Not found")
+
+(defrecord Language [name create])
+
+(get (Language. "Elixir" "José Valim") :name)
+
+(defrecord numbers [index middle ring pinky thumb])
