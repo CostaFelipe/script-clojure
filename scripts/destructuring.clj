83248ebe09b562e3cb730d74d4b90773f2f4a0ee
wedo <costadefelipe@gmail.com>
2025-06-19 08:59:54 -0300

    destructing

--- scripts/destructuring.clj
+++ scripts/destructuring.clj
@@ -19,3 +19,20 @@
 (let [{flower1 :flower1 :as all-flowers}
       {:flower1 "red"}]
   [flower1 all-flowers])
+
+(let [{:keys [flower1 flower2]}
+      {:flower1 "red" :flower2 "blue"}]
+  (str "The flowers are " flower1 " and " flower2))
+
+(defn flower-colors [colors]
+  (str "The flowers are "
+       (:flower1 colors)
+       " and "
+       (:flower2 colors)))
+
+(flower-colors {:flower1 "red" :flower2 "blue"})
+
+(defn flower-colors-one [{:keys [flower1 flower2]}]
+  (str "The flowers are " flower1 " and " flower2))
+
+(flower-colors-one {:flower1 "red" :flower2 "blue"})
