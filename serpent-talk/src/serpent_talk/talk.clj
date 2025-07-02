453d9924ed3da7637e80bfca60bd8e167a853eb2
wedo <costadefelipe@gmail.com>
2025-07-02 09:02:57 -0300

    teste & function serpent-talk

--- serpent-talk/src/serpent_talk/talk.clj
+++ serpent-talk/src/serpent_talk/talk.clj
@@ -1,4 +1,10 @@
-(ns serpent-talk.talk)
+(ns serpent-talk.talk
+  (:require [camel-snake-kebab.core :as csk]))
+
+(csk/->snake_case "hello pigeon")
+
+(defn serpent-talk [input]
+  (str "Serpent! You said: " (csk/->snake_case input)))
 
 (defn foo
   "I don't do a whole lot."
