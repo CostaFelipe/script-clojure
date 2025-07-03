44311898b8ba6eb86b366440ea214e55ed85bcdc
wedo <costadefelipe@gmail.com>
2025-07-03 08:53:49 -0300

    Serving Tea at a core.async Tea Party

--- async-tea-party/src/async_tea_party/core.clj
+++ async-tea-party/src/async_tea_party/core.clj
@@ -1,11 +1,39 @@
 (ns async-tea-party.core
   (:require [clojure.core.async :as async]))
 
+(def google-tea-service-chan (async/chan 10))
+(def yahoo-tea-service-chan (async/chan 10))
+
 (def tea-channel (async/chan 10))
 
 (def milk-channel (async/chan 10))
 
 (def sugar-channel (async/chan 10))
+
+(defn random-add []
+  (reduce + (conj [] (repeat 1 (rand-int 100000)))))
+
+(defn request-google-tea-service []
+  (async/go
+    (random-add)
+    (async/>! google-tea-service-chan
+              "tea compliments of google")))
+
+(defn request-yahoo-tea-service []
+  (async/go
+    (random-add)
+    (async/>! yahoo-tea-service-chan
+             "tea compliments of yahoo")))
+
+(defn request-tea []
+  (request-google-tea-service)
+  (request-yahoo-tea-service)
+  (async/go (let [[v] (async/alts!
+                       [google-tea-service-chan
+                        yahoo-tea-service-chan])]
+              (println v))))
+
+(request-tea)
 
 (async/go-loop []
   (println "Thanks for the " (async/<! tea-channel))
