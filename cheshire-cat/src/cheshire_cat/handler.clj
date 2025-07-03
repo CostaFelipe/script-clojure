7cccc41352706119e5b6795b9175e6556c160cc1
wedo <costadefelipe@gmail.com>
2025-07-03 16:19:58 -0300

    add new deps ring-json & add new router

--- cheshire-cat/src/cheshire_cat/handler.clj
+++ cheshire-cat/src/cheshire_cat/handler.clj
@@ -1,12 +1,27 @@
 (ns cheshire-cat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]
+            [ring.middleware.json :as ring-json]
+            [ring.util.response :as rr]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/cheshire-cat" [] "Smile!")
+  (GET "/" []
+    {:status 200
+     :head {"Content-Type" "application/json; charset=utf-8"}
+     :body "Hello World"})
+  (GET "/cheshire-cat" []
+    {:status 200
+     :head {"Content-Type" "application/json; charset=utf-8"}}
+    :body (json/generate-string
+           {:name "Cheshire Cat"
+            :status :grinning}))
+  (GET "/cheshire-json" []
+    (rr/response {:name "Cheshire Cat" :status :grinning}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      (ring-json/wrap-json-response)))
