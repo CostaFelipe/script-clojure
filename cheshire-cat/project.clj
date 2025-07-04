f1a7c3e5fb995a5f63bfc663cd071dd38a2549b0
wedo <costadefelipe@gmail.com>
2025-07-04 08:32:15 -0300

    add deps clojurescript

--- cheshire-cat/project.clj
+++ cheshire-cat/project.clj
@@ -6,7 +6,8 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [cheshire "6.0.0"]
-                 [ring/ring-json "0.5.1"]]
+                 [ring/ring-json "0.5.1"]
+                 [org.clojure/clojurescript "1.12.42"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
