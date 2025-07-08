446448bc697cd8cc8803318da8eb008fb98081bb
wedo <costadefelipe@gmail.com>
2025-07-08 13:32:47 -0300

    multimethod

--- scripts/multimethod.clj
+++ scripts/multimethod.clj
@@ -5,7 +5,15 @@
   (println "Assunto:" assunto)
   (println "Mensagem:" mensagem))
 
+(defmethod notificar :sms [{:keys [numero mensagem]}]
+  (println "Enviando SMS para" numero)
+  (println "Mensagem:" mensagem))
+
 (notificar {:tipo :email
             :destinatario "usuario@teste.com"
             :assunto "Promoção"
             :mensagem "Aproveite nossos descontos!"})
+
+(notificar {:tipo :sms
+            :numero "+5511987654321"
+            :mensagem "Seu código de verificação: 123456"})
