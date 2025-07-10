eaec0c2b1bff6b4de4cd4b79282b6ee099cf0ae3
wedo <costadefelipe@gmail.com>
2025-07-10 14:01:04 -0300

    multimethod

--- scripts/multimethod.clj
+++ scripts/multimethod.clj
@@ -9,7 +9,7 @@
   (println "Enviando SMS para" numero)
   (println "Mensagem:" mensagem))
 
-(notificar {:tipo :email
+(notificar {:tipo :emails
             :destinatario "usuario@teste.com"
             :assunto "Promoção"
             :mensagem "Aproveite nossos descontos!"})
@@ -17,3 +17,25 @@
 (notificar {:tipo :sms
             :numero "+5511987654321"
             :mensagem "Seu código de verificação: 123456"})
+
+(defmulti area (fn [shape & _] shape))
+
+(defmethod area :triangle [_ base height]
+  (/ (* base height) 2))
+
+(defmethod area :square [_ side]
+  (* side side))
+
+(defmethod area :rectangle [_ lenght width]
+  (* lenght width))
+
+(defmethod area :circle [_ radius]
+ (* radius radius Math/PI))
+
+(area :square 3)
+
+(area :triangle 3 4)
+
+(area :rectangle 4 6)
+
+(area :circle 5)
