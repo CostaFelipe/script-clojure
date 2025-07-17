3b2ddcb25719ce285a83e4abe0dd24e78c6d96b8
wedo <costadefelipe@gmail.com>
2025-07-17 16:25:57 -0300

    add record Item; add nova função criar-lista & adicionar-item

--- lista-de-compras/src/lista_de_compras/core.clj
+++ lista-de-compras/src/lista_de_compras/core.clj
@@ -1,5 +1,12 @@
 (ns lista-de-compras.core)
 
+(defrecord ItemCompras [nome quantidade categoria status])
+
+(defn criar-lista [] (atom []))
+
+(defn adicionar-item [lista item]
+  (swap! lista conj item))
+
 (defn foo
   "I don't do a whole lot."
   [x]
