dea80bca32ee602f833b912ea2de7a9476d703be
wedo <costadefelipe@gmail.com>
2025-07-17 17:00:32 -0300

    tentando corrgir um erro

--- lista-de-compras/src/lista_de_compras/core.clj
+++ lista-de-compras/src/lista_de_compras/core.clj
@@ -1,13 +1,18 @@
 (ns lista-de-compras.core)
 
-(defrecord ItemCompras [nome quantidade categoria status])
+(defrecord ItemCompra [nome quantidade preco categoria])
 
 (defn criar-lista [] (atom []))
 
 (defn adicionar-item [lista item]
   (swap! lista conj item))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main []
+  (let [lista criar-lista]
+    (adicionar-item lista (->ItemCompra "Arroz" 2 9.89 "Alimentos"))
+    (adicionar-item lista (->ItemCompra "Feijão" 3 8.75 "Alimentos"))
+    (adicionar-item lista (->ItemCompra "Sabonete" 5 2.50 "Higiene"))
+
+    (doseq [item @lista]
+      (println (str (:nome item) " - " (:quantidade item) " x R$ " (:preco item))))
+    ))
