8ea8df7b34260f05547711671c28a1a7e64b9763
wedo <costadefelipe@gmail.com>
2025-07-18 09:31:16 -0300

    add nova função: calcular-valor-item

--- lista-de-compras/src/lista_de_compras/lista.clj
+++ lista-de-compras/src/lista_de_compras/lista.clj
@@ -7,11 +7,16 @@
 (defn adicionar-item [lista item]
   (swap! lista conj item))
 
+(defn calcular-valor-item [item]
+  (* (:quantidade item) (:preco item)))
+
 (defn -main []
   (let [lista criar-lista]
     (adicionar-item lista (->ItemCompra "Arroz" 2 9.89 "Alimentos"))
     (adicionar-item lista (->ItemCompra "Feijão" 3 8.75 "Alimentos"))
     (adicionar-item lista (->ItemCompra "Sabonete" 5 2.50 "Higiene"))
+    @lista
 
-    (doseq [item @lista]
-      (println (str (:nome item) " - " (:quantidade item) " x R$ " (:preco item))))))
+    (calcular-valor-item (@lista 1))
+
+    ))
