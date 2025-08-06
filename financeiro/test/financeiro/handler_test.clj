b48d2f234d8186e8f1d501333d9f146719a0f3b3
wedo <costadefelipe@gmail.com>
2025-08-06 09:43:27 -0300

    refatorando os testes com midje

--- financeiro/test/financeiro/handler_test.clj
+++ financeiro/test/financeiro/handler_test.clj
@@ -1,14 +1,18 @@
 (ns financeiro.handler-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [ring.mock.request :as mock]
             [financeiro.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+(facts "Dá um 'Hello World' na rota raiz"
+       (fact "o status da resposta é 200"
+             (let [response (app (mock/request :get "/"))]
+               (:status response) => 200))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+       (fact "o texto do corpo é 'Hello World'"
+             (let [response (app (mock/request :get "/"))]
+               (:body response) => "Hello World")))
+
+(facts "Rota inválida não existe"
+       (fact "o código de erro é 404"
+             (let [response (app (mock/request :get "/invalid"))]
+               (:status response) => 404)))
