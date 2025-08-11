41d3a84fe6d97f15e50b774647efb61b279320a2
wedo <costadefelipe@gmail.com>
2025-08-11 09:19:32 -0300

    add new deps http & add new servidor jetty

--- financeiro/test/financeiro/saldo_aceitacao_test.clj
+++ financeiro/test/financeiro/saldo_aceitacao_test.clj
@@ -1,2 +1,17 @@
 (ns financeiro.saldo-aceitacao-test
-  (:require [ring.adapter.jetty :refer [run-jetty]]))
+  (:require [midje.sweet :refer :all]
+   [financeiro.handler :refer[app]]
+   [ring.adapter.jetty :refer [run-jetty]]))
+
+(def servidor (atom nil))
+
+(defn iniciar-servidor [porta]
+  (swap! servidor
+         (fn [_] (run-jetty app {:port porta :join? false}))))
+
+(defn parar-servidor []
+  (.stop @servidor))
+
+(fact "Iniciar e para o servidor"
+      (iniciar-servidor 3001)
+      (parar-servidor))
