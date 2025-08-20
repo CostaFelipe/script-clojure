6c4671b76998aec10c417c5b3b9042d849a13f4b
wedo <costadefelipe@gmail.com>
2025-08-20 15:15:11 -0300

    testes

--- financeiro/src/financeiro/handler.clj
+++ financeiro/src/financeiro/handler.clj
@@ -1,11 +1,12 @@
 (ns financeiro.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [cheshire.core :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/saldo" [] "0")
+  (GET "/saldo" [] (json/generate-string {:saldo 0}))
   (route/not-found "Not Found"))
 
 (def app
--- financeiro/test/aceitacao/saldo_aceitacao_test.clj
+++ financeiro/test/aceitacao/saldo_aceitacao_test.clj
@@ -1,8 +1,10 @@
 (ns saldo-aceitacao-test
   (:require [midje.sweet :refer :all]
+            [cheschire.core :as json]
     [auxiliares :refer :all]))
 
 (against-background [(before :facts (iniciar-servidor porta-padrao))
                      (after :facts (parar-servidor))]
  (fact "O saldo inicial é 0" :aceitacao
-      (conteudo "/saldo") => "0"))
+      ;;(conteudo "/saldo")
+       (json/parse-string (conteudo "/saldo")) => {:saldo 0}))
