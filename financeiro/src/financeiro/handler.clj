392cd85a8a4abde7d4652dbe06e660fbc2257239
wedo <costadefelipe@gmail.com>
2025-08-28 16:59:07 -0300

    test e new function

--- financeiro/src/financeiro/handler.clj
+++ financeiro/src/financeiro/handler.clj
@@ -2,17 +2,17 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [cheshire.core :as json]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defn saldo-como-json []
-  {:headers {"Content-Type"
-             "application/json; charset=utf-8"}
+  {:headers {"Content-Type" "application/json; charset=utf-8"}
    :body (json/generate-string {:saldo 0})})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/saldo" [] (saldo-como-json))
+  (POST "/transacoes" [] {})
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
